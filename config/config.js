affca05807ab3189198853c542f1fb980525b17e
Zehan Khan <zkhan@hootboard.com>
2023-04-21 00:54:28 +0530

    feat: :dizzy: monogoose added and config URI & pswd

--- config/config.js
+++ config/config.js
@@ -3,6 +3,10 @@ require("dotenv").config({ path: __dirname + "/../.env" });
 const config = {
   env: process.env.NODE_ENV,
   port: process.env.PORT || 3500,
+  db: {
+    url: process.env.MONGO_URI,
+    password: process.env.DATABASE_PASSWORD,
+  },
 };
 
 module.exports = config;
