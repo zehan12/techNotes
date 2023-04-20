d725b93240b6644f605b5a13bbf7f89b1b7d5d27
Zehan Khan <zkhan@hootboard.com>
2023-04-21 01:08:17 +0530

    feat: :label: database setup

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const server = require("./app");
 const config = require("./config/config");
+const connectDB = require("./config/db");
 
 const serverPort = config.port;
+
+connectDB();
 
 server.listen(serverPort, () => {
   console.log(`server is running port ${serverPort}`);
